66fae09cc3dc9a26176ba67cd19bd3e8e0e84d73
zhangchaowei <withqianqian@163.com>
2018-12-19 18:16:30 +0800

    创建 云充微信小程序

---
+++ src/pages/me/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <van-cell title="充电密码自动锁定" icon="location">
+      <van-switch :check="checked" @change="onChange"/>
+    </van-cell>
+
+    <van-cell title="充电记录" icon="location" is-link/>
+    <van-cell title="意见反馈" icon="location" is-link/>
+    <van-cell title="客服电话" icon="location" is-link/>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        checked: true
+      };
+    },
+    method:{
+      onChange() {
+        this.checked = this.checked ? false : true;
+      }
+    }
+
+  };
+</script>
+
+<style>
+</style>
